9a2a7c60e3b3e8106a8f58b1c847039e2723d54a
Dorin from home <comsaiuliand@gmail.com>
2020-12-28 20:59:55 +0200

    Pagina individuala pentru curente

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -38,16 +38,122 @@ menuButtons.forEach(menuButton=>{
 
         let tabTitle = document.getElementById('tab-title')
         tabTitle.innerText = menuButton.innerText
-        await loadCurente()
         loading.style.display='none'
         hideMenu()
     })
 })
 
 //LOAD CURENTE
+window.addEventListener('DOMContentLoaded',loadCurente)
+
 var curenteBTN = document.querySelector('[option=curente]')
 curenteBTN.addEventListener('click',loadCurente)
 async function loadCurente(){
-    let curente = await (await fetch('/getallcurente')).json()
-    
+    let curente = await (await fetch('/getcollection/curente')).json()
+    let curenteContainer = document.querySelector('[tab=curente]')
+    curenteContainer.innerHTML=''
+    curente.forEach(curent=>{
+        curenteContainer.insertAdjacentHTML('beforeend',renderCurent(curent))
+    })
+}
+function renderCurent(curent){
+    let renderedCurent = 
+    `
+    <div class="card flex-column">
+        <div class="top flex-row" style="background-color: var(--${curent.short})">
+            <h2 class="tit" style="color: ${curent.titlecolor}">${curent.name}</h2>
+        </div>
+        <div class="bottom flex-column">
+            <h3>${curent.perioada}</h3>
+            <p>${curent.definitie}</p>
+            <div class="flex-row ui">
+                <a href="/curente/${curent.short}">Read more</a>
+                <div class="flex-row">
+                    <p>Compara</p>
+                    <img src="../public/res/UI/check.svg"/>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    `
+    return renderedCurent
+}
+
+
+//LOAD PERIOADE
+var perioadeBTN = document.querySelector('[option=perioade]')
+perioadeBTN.addEventListener('click',loadPerioade)
+
+async function loadPerioade(){
+    let allperioade = await (await fetch('/getcollection/perioade')).json()
+    let perioadeContainer = document.querySelector('[tab=perioade]')
+    perioadeContainer.innerHTML=''
+
+    allperioade.forEach(perioada=>{
+        perioadeContainer.insertAdjacentHTML('beforeend',renderPerioada(perioada))
+    })
+}
+function renderPerioada(perioada){
+    let PERIOADA=
+    `
+    <div class="card flex-column">
+        <div class="top flex-row" style="background-color: var(--${perioada.short})">
+            <h2 class="tit" style="color: ${perioada.titlecolor}">${perioada.name}</h2>
+        </div>
+        <div class="bottom flex-column">
+            <h3>${perioada.date}</h3>
+            <p>${perioada.about}</p>
+            <div class="flex-row ui">
+                <a href="/perioade/${perioada.short}">Read more</a>
+                <div class="flex-row">
+                    <p>Compara</p>
+                    <img src="../public/res/UI/check.svg"/>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    `
+    return PERIOADA
+}
+
+//LOAD AUTORI
+var autoriBTN = document.querySelector('[option=autori]')
+autoriBTN.addEventListener('click', loadAutori)
+
+async function loadAutori(){
+    let allautori = await (await fetch('/getcollection/autori')).json()
+    let autoriContainer = document.querySelector('[tab=autori]')
+    autoriContainer.innerHTML=''
+
+    allautori.forEach(autor=>{
+        autoriContainer.insertAdjacentHTML('beforeend',renderAutor(autor))
+    })
+}
+
+function renderAutor(autor){
+    let AUTOR=
+    `
+    <div class="card flex-column">
+        <div class="top flex-row" style="background-image: url('../public/res/images/${autor.short}.jpg')" >
+            <div class="overlay" style="background-color: var(--${autor.perioada})"></div>
+            <h2 class="tit" >${autor.name}</h2>
+        </div>
+        <div class="bottom flex-column">
+            <h3>${autor.perioada}</h3>
+            <p>${autor.date}</p>
+            <div class="flex-row ui">
+                <a href="/autori/${autor.short}">Read more</a>
+                <div class="flex-row">
+                    <p>Compara</p>
+                    <img src="../public/res/UI/check.svg"/>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    `
+    return AUTOR;
+
 }
--- server.js
+++ server.js
@@ -6,17 +6,26 @@ var { Mongo } = require('./mongo')
 
 server.set('view engine','ejs')
 server.use('/public',express.static('public'))
-
+server.use(logger)
 
 server.get('/',function(req,res){
     res.render('main')
 })
 
-server.get('/getallcurente',async function(req,res){
-    let curente = await Mongo.db.db('bach').collection('curente').find({}).toArray()
-    res.json(curente)
+server.get('/getcollection/:collection',async function(req,res){
+    let collection = await Mongo.db.db('bach').collection(req.params.collection).find({enabled: {$ne :false}}).sort({order:1}).toArray()
+    res.json(collection)
 })
+server.get('/curente/:short', async function(req,res){
+    let curent = await Mongo.db.db('bach').collection('curente').findOne({short:req.params.short})
+    let autori = await Mongo.db.db('bach').collection('autori').find({curent:req.params.short}).toArray()
 
+    res.render('curente', {curent, autori})
+})
+function logger(req,res,next){
+    console.log(`[`+req.method+'] Request made at ['+new Date().toLocaleTimeString() + '] by [' + req.ip + '] for [' + req.url + ']')
+    next()
+}
 
 async function start(){
     await Mongo.connectToMongo()
